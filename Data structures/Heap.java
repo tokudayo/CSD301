c188964e6418d84a3ec5e73fb1b346d91da1b023
Tô Đức <20toduc01@gmail.com>
2021-06-01 20:52:13 +0700

    Minor changes

--- Data structures/Heap.java
+++ Data structures/Heap.java
@@ -1,36 +1,58 @@
 import static java.lang.Math.min;
 
+/**
+ * Implementation of integer min heap.
+ * The heap is initialized empty with a fixed size.
+ * It supports 2 main operation: insert and get the min element.
+ * @author To Duc
+ * @since 2021-6-1
+*/
 public class Heap {
     private int[] h; // Heap as array
     private int vacant = 1; // Next available position
 
-    public Heap(int[] arr) {
-        h = new int[arr.length*2];
-        for (int elem : arr) insert(elem);
+    public Heap(int size) {
+        h = new int[size];
     }
 
+    // Get the min element in the heap
     public int get () {
+        if (vacant == 1) return 0;
+        return h[1];
+    }
+
+    // Get and remove the min element in the heap
+    public int pop () {
+        if (vacant == 1) return 0;
         int globalMin = h[1];
         h[1] = h[--vacant];
-        top_down(1);
+        topDown(1);
         return globalMin;
     }
 
+    // Insert an element to the heap
     public void insert (int x) {
         h[vacant] = x;
-        bottom_up(vacant++);
+        bottomUp(vacant++);
     }
 
-    private void bottom_up (int node) {
+    // Insert an array of element to the heap
+    public void insert(int[] arr) {
+        for (int elem : arr) insert(elem);
+    }
+
+    // Bottom-up heapify
+    private void bottomUp (int node) {
         if (node == 0) return;
         int parent = node/2;
         if (h[parent] > h[node]) {
             swap(parent, node);
-            bottom_up(parent);
+            bottomUp(parent);
         }
     }
 
-    private void top_down (int node) {
+    // Top-down heapify
+    private void topDown (int node) {
         int lChild = node*2;
         int rChild = node*2 + 1;
 
@@ -41,7 +63,7 @@ public class Heap {
         if (rChild == vacant) {
             if (h[lChild] < h[node]) {
                 swap(lChild, node);
-                top_down(lChild);
+                topDown(lChild);
             }
         }
 
@@ -50,14 +72,15 @@ public class Heap {
         branchMin = min(branchMin, h[node]);
         if (branchMin == h[lChild]) {
             swap(lChild, node);
-            top_down(lChild);
+            topDown(lChild);
         }
         else if (branchMin == h[rChild]) {
             swap(rChild, node);
-            top_down(rChild);
+            topDown(rChild);
         }
     }
 
+    // Swap the value of 2 heap nodes
     private void swap(int posA, int posB) {
         int buffer = h[posA];
         h[posA] = h[posB];
